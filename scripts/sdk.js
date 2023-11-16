48515cd983c732ed22135db09c40e71b8faa6ccf
Pierre Vanoverschelde <pierre.vano@gmail.com>
2023-11-16 15:23:28 +0100

    style: add prettier and remove vmonkey folder

--- scripts/sdk.js
+++ scripts/sdk.js
@@ -1,8 +1,7 @@
-
 function writeSDK(apikey, noticeid, global, staging) {
-  var _staging = staging ? 'staging.' : '';
+  var _staging = staging ? "staging." : "";
   window.gdprAppliesGlobally = global;
-  (function() {
+  (function () {
     function r() {
       if (!window.frames.__cmpLocator) {
         if (document.body && document.body.firstChild) {
@@ -11,74 +10,77 @@ function writeSDK(apikey, noticeid, global, staging) {
           t.style.display = "none";
           t.name = "__cmpLocator";
           t.title = "cmpLocator";
-          e.insertBefore(t, e.firstChild)
+          e.insertBefore(t, e.firstChild);
         } else {
-          setTimeout(r, 5)
+          setTimeout(r, 5);
         }
       }
     }
 
     function e(e, t, r) {
       if (typeof r !== "function") {
-        return
+        return;
       }
       if (!window.__cmpBuffer) {
-        window.__cmpBuffer = []
+        window.__cmpBuffer = [];
       }
       if (e === "ping") {
-        r({
-          gdprAppliesGlobally: window.gdprAppliesGlobally,
-          cmpLoaded: false
-        }, true)
+        r(
+          {
+            gdprAppliesGlobally: window.gdprAppliesGlobally,
+            cmpLoaded: false,
+          },
+          true,
+        );
       } else {
         window.__cmpBuffer.push({
           command: e,
           parameter: t,
-          callback: r
-        })
+          callback: r,
+        });
       }
     }
     e.stub = true;
 
     function t(a) {
       if (!window.__cmp || window.__cmp.stub !== true) {
-        return
+        return;
       }
       if (!a.data) {
-        return
+        return;
       }
       var n = typeof a.data === "string";
       var e;
       try {
-        e = n ? JSON.parse(a.data) : a.data
+        e = n ? JSON.parse(a.data) : a.data;
       } catch (t) {
-        return
+        return;
       }
       if (e.__cmpCall) {
         var o = e.__cmpCall;
-        window.__cmp(o.command, o.parameter, function(e, t) {
+        window.__cmp(o.command, o.parameter, function (e, t) {
           var r = {
             __cmpReturn: {
               returnValue: e,
               success: t,
-              callId: o.callId
-            }
+              callId: o.callId,
+            },
           };
-          a.source.postMessage(n ? JSON.stringify(r) : r, "*")
-        })
+          a.source.postMessage(n ? JSON.stringify(r) : r, "*");
+        });
       }
     }
     if (typeof window.__cmp !== "function") {
       window.__cmp = e;
       if (window.addEventListener) {
-        window.addEventListener("message", t, false)
+        window.addEventListener("message", t, false);
       } else {
-        window.attachEvent("onmessage", t)
+        window.attachEvent("onmessage", t);
       }
     }
-    r()
+    r();
   })();
-  (function(e, t) {
+  (function (e, t) {
     var r = document.createElement("script");
     r.id = "spcloader";
     r.type = "text/javascript";
@@ -86,6 +88,6 @@ function writeSDK(apikey, noticeid, global, staging) {
     r.src = "https://sdk." + _staging + "privacy-center.org/" + e + "/loader.js?target_type=notice&target=" + t;
     r.charset = "utf-8";
     var a = document.getElementsByTagName("script")[0];
-    a.parentNode.insertBefore(r, a)
+    a.parentNode.insertBefore(r, a);
   })(apikey, noticeid);
 }
